8c5c7a7b2339c6f5dbfe99f2835d57dd24a5bd4f
piwriw <piwriw@163.com>
2023-10-16 17:26:23 +0800

    [feat]:add harbor & more check docker

--- k8s-setup-offline/docker-install.sh
+++ k8s-setup-offline/docker-install.sh
@@ -10,14 +10,18 @@ set -e
 # https://docs.docker.com/install/linux/linux-postinstall/
 
 # 卸载旧版本
-yum remove docker \
+yum remove  -y docker \
     docker-client \
     docker-client-latest \
     docker-common \
     docker-latest \
     docker-latest-logrotate \
     docker-logrotate \
-    docker-engine
+    docker-engine \
+    docker-ce \
+    docker-ce-cli \
+    docker-buildx-plugin
+
 
 # 安装并启动 docker
 yum install -y docker-ce-20.10.0 docker-ce-cli-20.10.0 containerd.io
