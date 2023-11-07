2c7439d0c16e280eeb3c5b71740e06f0d722ea44
piwriw <piwriw@163.com>
2023-11-07 14:01:02 +0800

    [fix]:fix loss calico.yaml

--- k8s-setup-offline/tag_all.sh
+++ k8s-setup-offline/tag_all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set  -e
 
- tar -czvf K8s-offline-master.tar.gz Readme.md     clear-k8s.sh        docker-install.sh   images/master    \
+ tar -czvf k8s-offline-master.tar.gz Readme.md     clear-k8s.sh     yaml   docker-install.sh   images/master    \
           k8s-setup.sh        package             repo-setup.sh             \
           clear-docker.sh     docker-conf.sh      homename-setup.sh   k8s-help-install.sh load-image.sh     \
           push-file.sh        set-up-master.sh    tag_all.sh
 
- tar -czvf K8s-offline-work.tar.gz Readme.md      clear-k8s.sh        docker-install.sh   images/work   \
+ tar -czvf k8s-offline-work.tar.gz Readme.md      clear-k8s.sh      yaml  docker-install.sh   images/work   \
           k8s-setup.sh        package             repo-setup.sh       set-up-work.sh       \
           clear-docker.sh     docker-conf.sh      homename-setup.sh   k8s-help-install.sh load-image.sh     \
           push-file.sh         tag_all.sh
