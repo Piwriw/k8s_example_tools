971188ebb41b185ad061879b4c21b68cf5cae7e8
piwriw <piwriw@163.com>
2023-11-14 16:15:29 +0800

    [doc]:add 09-stopwalld.sh

--- k8s-auto-joindisjoin-offline-work/01-setup_work.sh
+++ k8s-auto-joindisjoin-offline-work/01-setup_work.sh
@@ -53,6 +53,7 @@ doScript(){
         bash ./05-load-image.sh "images/work"
         bash ./06-k8s-setup.sh
         bash ./07-work-join.sh
+        bash ./09-stopwalld.sh
      elif [ "$WORK_MODEL" = "disjoin"  ]; then
         bash ./08-work-disjoin.sh
       else
