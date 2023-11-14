597f210a99caf84d6dc18faae964ece35b3d1a3b
piwriw <piwriw@163.com>
2023-11-14 15:42:53 +0800

    [chore]:check docker daemon.json utils

---
+++ cmd/docker_daemon_utils/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type DockerConfig struct {
+	ExecOpts           []string `json:"exec-opts"`
+	InsecureRegistries []string `json:"insecure-registries"`
+	RegistryMirrors    []string `json:"registry-mirrors"`
+}
+
+const DockerDaemonPath = "/etc/docker/daemon.json"
+
+func main() {
+	dockerConfig, daemonSourceMap, err := readJSONFile(DockerDaemonPath)
+	if err != nil {
+		panic(err)
+	}
+	dockerConfig.ExecOpts = CheckConfig(dockerConfig.ExecOpts, "native.cgroupdriver=systemd")
+	dockerConfig.InsecureRegistries = CheckConfig(dockerConfig.InsecureRegistries, "0.0.0.0/0")
+	dockerConfig.RegistryMirrors = CheckConfig(dockerConfig.RegistryMirrors, os.Getenv("HARBOR_ADDR"))
+
+	daemonSourceMap["exec-opts"] = dockerConfig.ExecOpts
+	daemonSourceMap["insecure-registries"] = dockerConfig.InsecureRegistries
+	daemonSourceMap["registry-mirrors"] = dockerConfig.RegistryMirrors
+	bytes, err := json.MarshalIndent(daemonSourceMap, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	if err = os.WriteFile(DockerDaemonPath, bytes, 452); err != nil {
+		panic(err)
+	}
+
+}
+func CheckConfig(configs []string, target string) []string {
+	found := false
+	for _, registry := range configs {
+		if registry == target {
+			found = true
+			break
+		}
+	}
+	if !found {
+		configs = append(configs, target)
+	}
+	return configs
+}
+func readJSONFile(dirPath string) (*DockerConfig, map[string]interface{}, error) {
+	// 读取目录
+	daemonSource, err := os.ReadFile(dirPath)
+	if err != nil {
+		return nil, nil, err
+	}
+	dockerConf := &DockerConfig{}
+	var daemonSourceMap map[string]interface{}
+	if err := json.Unmarshal(daemonSource, &daemonSourceMap); err != nil {
+		return nil, nil, err
+	}
+	if err = json.Unmarshal(daemonSource, dockerConf); err != nil {
+		return nil, nil, err
+	}
+	return dockerConf, daemonSourceMap, err
+}
