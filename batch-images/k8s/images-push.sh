f5e79023f9f4cfd69acf43c75e95de03ba9b1aa2
piwriw <piwriw@163.com>
2024-02-21 09:32:53 +0800

    [fix]:fix the name of Harbor

--- batch-images/k8s/images-push.sh
+++ batch-images/k8s/images-push.sh
@@ -4,23 +4,23 @@ set  -e
 usage() {
     echo "使用说明："
     echo "导入以下参数再执行images-push"
-    echo "export DOCKER_USERNAME=admin"
-    echo "export DOCKER_PASSWORD=Harbor12345"
+    echo "export HARBOR_USERNAME=admin"
+    echo "export HARBOR_PASSWORD=Harbor12345"
     echo "export HARBOR_ADDR=192.168.10.10"
     echo ""
 }
 
 printEnv() {
     echo "已设置环境变量参数："
-    echo "DOCKER_USERNAME="${DOCKER_USERNAME}
-    echo "DOCKER_PASSWORD="${DOCKER_PASSWORD}
+    echo "HARBOR_USERNAME="${HARBOR_USERNAME}
+    echo "HARBOR_PASSWORD="${HARBOR_PASSWORD}
     echo "HARBOR_ADDR="${HARBOR_ADDR}
     echo ""
 }
 
 doPushImages(){
 # 登录到Docker仓库
-docker login -u "$DOCKER_USERNAME" -p $DOCKER_PASSWORD $HARBOR_ADDR
+docker login -u "$HARBOR_USERNAME" -p $HARBOR_PASSWORD $HARBOR_ADDR
 
 # 循环遍历镜像列表，并推送到Docker仓库
 docker images --format "{{.Repository}}:{{.Tag}}" | while read -r image; do
@@ -37,7 +37,7 @@ done
 docker logout
 }
 
-if [ ${#DOCKER_USERNAME} -eq 0 ] ||  [ ${#DOCKER_PASSWORD} -eq 0 ]  || [ ${#HARBOR_ADDR} -eq 0 ] ; then
+if [ ${#HARBOR_USERNAME} -eq 0 ] ||  [ ${#HARBOR_PASSWORD} -eq 0 ]  || [ ${#HARBOR_ADDR} -eq 0 ] ; then
     usage
     echo -e "\033[31;1m缺少环境变量参数 \033[0m"
     printEnv
